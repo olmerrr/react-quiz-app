fa85bfaa375d84722bb1293e2fb67353382f0f04
Yaroslav Dombrovskyi <dombrovskiy91@gmail.com>
2024-03-05 16:53:05 +0100

    fix bug with ButtonNext
    fix bug with ButtonNext

--- src/App.js
+++ src/App.js
@@ -6,40 +6,38 @@ import Loader from "./components/Loader";
 import Error from "./components/Error";
 import StartScreen from "./components/StartScreen";
 import Question from "./components/Question";
+import NextButton from "./components/NextButton";
 
 const initialState = {
-  questions: [],
-  status: "loading", // "loading", "error", "ready", "active", "finished"
-  index: 0,
-  answer: null,
-  points: 0,
+  questions: [], status: "loading", // "loading", "error", "ready", "active", "finished"
+  index: 0, answer: null, points: 0,
 }
 
 function reducer(state, action) {
   switch ( action.type ) {
     case "dataReceived":
       return {
-        ...state,
-        questions: action.payload,
-        status: "ready"
+        ...state, questions: action.payload, status: "ready"
       }
     case "start":
       return {
-        ...state,
-        status: "active"
+        ...state, status: "active"
       }
     case "newAnswer":
-      const question = state.questions.at(state.index)
+      const question = state.questions.at(state.index);
+      return {
+        ...state, answer: action.payload, // calculate points
+        points: action.payload === question.correctOption ? state.points + question.points : state.points
+      }
+    case "nextQuestion":
       return {
         ...state,
-        answer: action.payload,
-        // calculate points
-        points: action.payload === question.correctOption ? state.points + question.points : state.points
+        index: state.index + 1,
+        answer: null
       }
     case "dataFailed":
       return {
-        ...state,
-        status: "error"
+        ...state, status: "error"
       }
     default: {
       throw new Error("Action is unknown")
@@ -74,12 +72,15 @@ const App = () => {
       {status === "loading" && <Loader/>}
       {status === "error" && <Error/>}
       {status === "ready" && <StartScreen numQuestions={numQuestions} dispatch={dispatch}/>}
-      {status === "active" && <Question
-        question={questions[index]}
-        dispatch={dispatch}
-        answer={answer}
-        points={points}
-      />}
+      {status === "active" && <>
+        <Question
+          question={questions[index]}
+          dispatch={dispatch}
+          answer={answer}
+          points={points}
+        />
+        <NextButton dispatch={dispatch} answer={answer}/>
+      </>}
     </Main>
   </div>
 }
