3d42bb5052befc3243e48f23e97306c2f2cb2e8f
Yaroslav Dombrovskyi <dombrovskiy91@gmail.com>
2024-03-12 11:05:41 +0100

    add timer + more logic.
    add timer + more logic.

--- src/App.js
+++ src/App.js
@@ -8,11 +8,20 @@ import StartScreen from "./components/StartScreen";
 import Question from "./components/Question";
 import NextButton from "./components/NextButton";
 import Progress from "./components/Progress";
+import Timer from "./components/Timer";
+import FinishScreen from "./components/FinishScreen";
+import { Footer } from "./components/Footer";
 
 const initialState = {
-  questions: [], status: "loading", // "loading", "error", "ready", "active", "finished"
-  index: 0, answer: null, points: 0,
+  questions: [],
+  status: "loading", // "loading", "error", "ready", "active", "finished"
+  index: 0,
+  answer: null,
+  points: 0,
+  highscore: 0,
+  secondsRemaining: null
 }
+const SECONDS_PER_QUESTION = 30;
 
 function reducer(state, action) {
   switch ( action.type ) {
@@ -22,7 +31,15 @@ function reducer(state, action) {
       }
     case "start":
       return {
-        ...state, status: "active"
+        ...state,
+        status: "active",
+        secondsRemaining: state.questions.length * SECONDS_PER_QUESTION
+      }
+    case "tick":
+      return {
+        ...state,
+        secondsRemaining: state.secondsRemaining - 1,
+        status: state.secondsRemaining === 0 ? "finished" : state.status
       }
     case "newAnswer":
       const question = state.questions.at(state.index);
@@ -36,6 +53,18 @@ function reducer(state, action) {
         index: state.index + 1,
         answer: null
       }
+    case "finish":
+      return {
+        ...state,
+        status: "finished",
+        highscore: state.points > state.highscore ? state.points : state.highscore
+      }
+    case "restart":
+      return {
+        ...initialState,
+        questions: state.questions,
+        status: "ready"
+      }
     case "dataFailed":
       return {
         ...state, status: "error"
@@ -47,7 +76,15 @@ function reducer(state, action) {
 }
 
 const App = () => {
-  const [ { questions, status, index, answer, points }, dispatch ] = useReducer(reducer, initialState);
+  const [ {
+    questions,
+    status,
+    index,
+    answer,
+    points,
+    highscore,
+    secondsRemaining
+  }, dispatch ] = useReducer(reducer, initialState);
   const numQuestions = questions.length;
   const maxPossiblePoints = questions.reduce((prev, cur) => prev + cur.points, 0);
 
@@ -89,8 +126,22 @@ const App = () => {
           answer={answer}
           points={points}
         />
-        <NextButton dispatch={dispatch} answer={answer}/>
+        <NextButton
+          dispatch={dispatch}
+          answer={answer}
+          numQuestions={numQuestions}
+          index={index}
+        />
+        <Footer>
+          <Timer dispatch={dispatch} secondsRemaining={secondsRemaining}/>
+        </Footer>
       </>}
+      {status === "finished" && <FinishScreen
+        points={points}
+        maxPossiblePoints={maxPossiblePoints}
+        highscore={highscore}
+        dispatch={dispatch}
+      />}
     </Main>
   </div>
 }
---
+++ src/components/Footer.js
@@ -0,0 +1,5 @@
+export function Footer({ children }) {
+  return <footer>
+    {children}
+  </footer>
+}
